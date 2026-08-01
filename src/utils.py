1b9f0060f382c22e17842d32f8a1ea573a1c39bf
nazifatahsinturna <nazifatahsinturnabd@gmail.com>
2026-08-01 19:55:37 -0400

    added basic error handling to the functions

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,14 @@
 def add(a, b):
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+        return "Both a and b must be numbers"
     return a+b
 
 def subtract(a, b):
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+            return "Both a and b must be numbers"
     return a-b
 
 def multiply(a, b):
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+            return "Both a and b must be numbers"
     return a*b
